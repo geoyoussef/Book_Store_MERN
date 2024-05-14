63d0813a88a9cbe87113beebf2d3bab21d32e938
Yassa122 <yassa.ashraf@yahoo.com>
2024-05-14 19:36:56 +0300

    moved all the keys and id to env and added a print statement to ensure the validty of access key and secret

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,36 @@
-import express, { request, response } from "express";
-import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
-import booksRoute from "./routes/booksRoute.js";
+import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import booksRoute from "./routes/booksRoute.js";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5555;
 
 // Middleware for parsing request body
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: "https://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
 
-app.get("/", (request, response) => {
-  console.log(request);
-  return response.status(234).send("Welcome to MERN Stack Book Shop");
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(200).send("Welcome to MERN Stack Book Shop");
 });
 
 app.use("/books", booksRoute);
 
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log("App connected to db");
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}!`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// Log environment variables to verify they are loaded correctly
+console.log("AWS_REGION:", process.env.AWS_REGION);
+console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
+console.log(
+  "AWS_SECRET_ACCESS_KEY:",
+  process.env.AWS_SECRET_ACCESS_KEY ? "*****" : "Not set"
+);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
